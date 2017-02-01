c7923b94fa05f3c1de8b3cde6d75d17d99d24152
garethquirke <gareth.quirke@gmail.com>
2017-02-01 23:27:21 +0000

    pseudocode for problem

--- Labs/Lab 2/main.cpp
+++ Labs/Lab 2/main.cpp
@@ -29,26 +29,43 @@ void multiples(T& sum, T x, int n) {
 	}
 	cout << endl;
 }
-template<class T1>
-T1 init(T1 num1, T1 num2, T2&start) {
 
+// init sets the value of the parameter start to a T2 - type value of 1
+// returns a T1 - type value which is the sum of num1 and num2
+template<class T1, class T2>
+T1 init(T1 num1, T1 num2, T2&start) {
+	start = (T2) 1;
+	return (num1 + num2);
 }
 /*
-1.	Create the C++ Function Template named init so that it has three
-parameters whose types are determined by the function template type
-parameters T1 and T2.  The function header is shown below.  init sets
-the value of the parameter start to a T2-type value of 1.  init
-returns a T1-type value which is the sum of num1 and num2.
-
-T1 init (T1 num1, T1 num2, T2& start)
-
 Create a template function for the QuickSort algorithm  (http://en.wikipedia.org/wiki/Quicksort)
 
 template<typename T>
 T* quicksort(T* array)
 
 */
+template<class T>
+T parition(T* array, int start, int end) {
+	
+}
 
+template<class T>
+T* quicksort(T* array) {
+	// wall, pivot  need to be abstracted first
+
+	// from the current element create the quick sort algorithm
+
+	/*
+	Quicksort is an algorithm to sort a list. It is more efficient in most cases.
+	O(n log n) ---> its big o notation
+
+	test data: 6,5,1,3,8,4,7,9,2
+	pivot is 2
+	wall is to the left of 6
+	current element is 6
+
+	*/
+}
 int main() {
 	// swap the values back and forth through the two template functions
 	int a = 10 , b = 20;
@@ -66,6 +83,7 @@ int main() {
 
 	int sum, x = 5, n = 10;
 	multiples(sum, x, n);
+
 
 	system("pause");
 	return 0;
