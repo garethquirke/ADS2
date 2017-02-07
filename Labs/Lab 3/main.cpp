0b50dbe60271ffd3de129e4afb2e089ecc29dfe0
garethquirke <gareth.quirke@gmail.com>
2017-02-07 20:39:14 +0000

    lab 3 discussed

---
+++ Labs/Lab 3/main.cpp
@@ -0,0 +1,13 @@
+/*
+Create a merge function template:
+- merges he contents of an array that has been sorted already
+
+merge(int aarraySortedInTwoHalves[], int startIndex, int length)
+*/
+
+/*
+Create a mergeSort
+- it uses the merge function
+- sorts an array of integers
+- recursive call to the merge function
+*/
