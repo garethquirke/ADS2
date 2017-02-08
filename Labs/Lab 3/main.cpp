94692cb4e0984795c2cd888a3b1d16dc27f8bd0d
garethquirke <gareth.quirke@gmail.com>
2017-02-08 21:00:40 +0000

    u

--- Labs/Lab 3/main.cpp
+++ Labs/Lab 3/main.cpp
@@ -1,13 +1,77 @@
-/*
-Create a merge function template:
-- merges he contents of an array that has been sorted already
+// Author: Gareth Quirke
+// Date: 7th February 2017
 
-merge(int aarraySortedInTwoHalves[], int startIndex, int length)
-*/
+#include <iostream>
+using namespace std;
 
-/*
-Create a mergeSort
-- it uses the merge function
-- sorts an array of integers
-- recursive call to the merge function
-*/
+// Declarations
+void merge(int array[], int start, int length);
+void mergeSort(int array[], int start, int length);
+
+
+void mergeSort(int array[], int start, int length)
+{
+	int mid;
+
+
+	if (start >= length)
+	{
+		return;
+	}
+
+	mid = (start + length) / 2;
+
+	mergeSort(array, start, mid);
+
+	mergeSort(array, mid + 1, length);
+	merge(array, start, length);
+}
+
+void merge(int array[], int start, int length)
+{
+
+	int size = (length - start) + 1;
+	int *temp = new int[size]();
+
+	int current = start;
+	int mid = (start + length) / 2;
+
+	int x = 0;
+	int y = mid + 1;
+
+	while (x < size)
+	{
+		if ((current <= mid) && (array[current] < array[y]))
+		{
+			temp[x++] = array[current++];
+		}
+		else
+		{
+			temp[x++] = array[y++];
+		}
+
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		array[start + x] = temp[x];
+	}
+
+	delete[]temp;
+
+}
+
+int main()
+{
+	int array[10] = { 9,5,8,4,7,2,6,3,2,10 };
+
+	mergeSort(array, 0, 9);
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << array[i] << endl;
+	}
+	system("pause");
+	return 0;
+}
